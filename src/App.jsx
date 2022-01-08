333b311c0720740f2bdfb2ecd91aea17f180490d
Cauan Boss <cauanboss@gmail.com>
2022-01-08 13:40:47 -0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('EFFECT', new Date().toLocaleString());
+
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        throw e;
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return [result, loading];
+};
+
+const App = () => {
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!loading && result) {
+    console.log(result);
+  }
+
+  return <h1>Oi</h1>;
+};
+
+export default App;
